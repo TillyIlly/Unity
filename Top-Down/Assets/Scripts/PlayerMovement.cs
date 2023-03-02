388d7ed396fa3b86cb71d24b9ee56bb8e2963be3
TillyIlly <Andrew_Holden10@yahoo.com>
2023-03-02 17:09:43 -0600

    Added Movement

--- Top-Down/Assets/Scripts/PlayerMovement.cs
+++ Top-Down/Assets/Scripts/PlayerMovement.cs
@@ -5,10 +5,24 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     
+    public float moveSpeed = 5f;
 
-    // Update is called once per frame
+    public Rigidbody2D rb;
+    public Animator animator;
+    Vector2 movement;
+    
     void Update()
     {
-        
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
     }
 }
