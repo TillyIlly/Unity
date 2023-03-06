3144bdb8029d56609cb2e2c00610b8c07c26be0d
Isaac <isaacguid.24@gmail.com>
2023-03-05 21:14:26 -0600

    Update PlayerMovement.cs

--- Top-Down/Assets/Scripts/PlayerMovement.cs
+++ Top-Down/Assets/Scripts/PlayerMovement.cs
@@ -26,3 +26,47 @@ public class PlayerMovement : MonoBehaviour
         rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
     }
 }
+
+/*
+
+possible alternative for movement script
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed;
+    public Rigidbody2D rb;
+
+    private Vector2 moveDirection;
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInputs();
+    }
+
+    void FixedUpdate()
+    {
+        Move();
+        //Physics calculations
+    }
+
+    void ProcessInputs()
+    {
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+
+        moveDirection = new Vector2(moveX, moveY).normalized;
+    }
+
+    void Move()
+    {
+        rb.velocity = new Vector2(moveDirection.x * moveSpeed, moveDirection.y * moveSpeed);
+
+    }
+}
+*/
