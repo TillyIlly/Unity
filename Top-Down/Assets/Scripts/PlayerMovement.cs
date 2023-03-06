2f45badb358041d25f55ce5279129ee9add5bc64
Isaac <isaacguid.24@gmail.com>
2023-03-06 12:31:24 -0600

    tired to rework movement

--- Top-Down/Assets/Scripts/PlayerMovement.cs
+++ Top-Down/Assets/Scripts/PlayerMovement.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+/*using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -25,11 +25,11 @@ public class PlayerMovement : MonoBehaviour
     {
         rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
     }
-}
+}*/
 
-/*
 
-possible alternative for movement script
+
+//possible alternative for movement script
 
 using System.Collections;
 using System.Collections.Generic;
@@ -39,14 +39,26 @@ public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed;
     public Rigidbody2D rb;
+    public Animator animator;
+    private SpriteRenderer sprite;
 
+    private float moveX = 0f;
     private Vector2 moveDirection;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        sprite = GetComponent<SpriteRenderer>();
+    }
     
 
     // Update is called once per frame
     void Update()
     {
         ProcessInputs();
+        animator.SetFloat("Horizontal", moveDirection.x);
+        animator.SetFloat("Vertical", moveDirection.y);
+        animator.SetFloat("Speed", moveDirection.sqrMagnitude);
     }
 
     void FixedUpdate()
@@ -57,7 +69,7 @@ public class PlayerMovement : MonoBehaviour
 
     void ProcessInputs()
     {
-        float moveX = Input.GetAxisRaw("Horizontal");
+        moveX = Input.GetAxisRaw("Horizontal");
         float moveY = Input.GetAxisRaw("Vertical");
 
         moveDirection = new Vector2(moveX, moveY).normalized;
@@ -65,8 +77,16 @@ public class PlayerMovement : MonoBehaviour
 
     void Move()
     {
+        sprite.flipX = false;
+        if (moveX > 0f)
+            sprite.flipX = false;
+
+        // running in left direction
+        else if (moveX < 0f)
+            sprite.flipX = true;
+
         rb.velocity = new Vector2(moveDirection.x * moveSpeed, moveDirection.y * moveSpeed);
+       
 
     }
 }
-*/
