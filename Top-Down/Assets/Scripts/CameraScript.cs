de8d3178d692c987355818672626636267835b66
Isaac <isaacguid.24@gmail.com>
2023-03-06 15:29:13 -0600

    idk

--- Top-Down/Assets/Scripts/CameraScript.cs
+++ Top-Down/Assets/Scripts/CameraScript.cs
@@ -15,6 +15,8 @@ public class CameraScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = new Vector3(target.transform.position.x, target.transform.position.y, transform.position.z);
+  
+            transform.position = new Vector3(target.transform.position.x, target.transform.position.y, transform.position.z);
+
     }
 }
